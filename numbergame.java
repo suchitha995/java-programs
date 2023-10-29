1cd27f831abdb43b42895467e4b4f550bd45225e
suchitha995 <136322678+suchitha995@users.noreply.github.com>
2023-10-29 12:18:54 +0530

    Create numbergame.java

---
+++ numbergame.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class numbergame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int maxAttempts = 10;
+        int rounds = 0;
+        int score = 0;
+
+        System.out.println(" Number Guessing Game!");
+
+        while (true) {
+            int numberToGuess = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+
+            System.out.println("Round " + (rounds + 1));
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == numberToGuess) {
+                    System.out.println(" You've guessed the correct number in " + attempts + " attempts.");
+                    score++;
+                    break;
+                } else if (userGuess < numberToGuess) {
+                    System.out.println("Your guess is too low.");
+                } else {
+                    System.out.println("Your guess is too high.");
+                }
+
+                if (attempts == maxAttempts) {
+                    System.out.println("Sorry, you've run out of attempts. The correct number was " + numberToGuess + ".");
+                }
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgain = scanner.next().toLowerCase();
+            if (!playAgain.equals("yes")) {
+                break;
+            }
+            rounds++;
+        }
+
+        System.out.println("Game over. You played " + rounds + " rounds and your total score is " + score);
+    }
+}
